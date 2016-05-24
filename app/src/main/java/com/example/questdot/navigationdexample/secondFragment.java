411f8a2950533c2143e6a8b2a010b6d3fb4e35f4
HP <krist.ng@corous360.com>
2016-05-24 19:54:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/questdot/navigationdexample/secondFragment.java
@@ -0,0 +1,32 @@
+package com.example.questdot.navigationdexample;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class secondFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    public secondFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+
+
+}
